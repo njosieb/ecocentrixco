75d9ea5abaeb55ba0c75bfaf947a372d1d246c3e
Nina Harris <ninaharris121@gmail.com>
2019-08-03 11:29:44 -0500

    Adds div where map will show on project template

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -12,19 +12,24 @@ export default function Template({
   return (
     <Layout>
       <div className="blog-post-container">
-        <div className="blog-post pb5">
+        <div className="blog-post">
           <div className="pb0-ns relative z-1 h5">
             <div className="bg-blue absolute cover h-100 w-100 z-5" />
             <h1 className="project-header mw8 center mv0 z-5 relative white tc">
               {title}
             </h1>
           </div>
-          <div className="container fw5">
-            <l className="green f3">
-              <li className="pt4">{city + ', ' + state}</li>
-              <li>{units + ' Units'}</li>
-              <li>{type + ': ' + tags}</li>
-            </l>
+          <div className="container fw5 flex justify-between center project-info">
+            <div>
+              <ul className="black f4 pl0">
+                <li>{'Location: ' + city + ', ' + state}</li>
+                <li>{'Project Type: ' + tags + ' (' + type + ')'}</li>
+                <li>{'Number of Units: ' + units}</li>
+              </ul>
+            </div>
+            <div className="map pa2">
+              <p className="f3">Map showing location will go here!</p>
+            </div>
           </div>
         </div>
         <div
